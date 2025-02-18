2839b37aa11b7b03558824327d58d6d8209a9094
changwenbo <123456@qq.com>
2025-02-18 18:28:49 +0800

    123

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,28 @@
+const fs = require('fs');
+
+try {
+      const key = fs.readFileSync('/usr/local/nginx/key.pem');
+        const cert = fs.readFileSync('/usr/local/nginx/cert.pem');
+          console.log('Key and Cert read successfully');
+} catch (err) {
+      console.error('Error reading key or cert:', err);
+}
+
 module.exports = {
   publicPath: './',
   devServer: {
+    host: '0.0.0.0',
     port: 5656,
+    allowedHosts: 'all',
+    https: {
+        key: fs.readFileSync('/usr/local/nginx/key.pem'),
+        cert: fs.readFileSync('/usr/local/nginx/cert.pem'),
+    },
     proxy: {
       '/wt': {
-        target: 'http://127.0.0.1:5656',
+        target: 'https://localhost:5656',
         ws: true,
-        changeOrigin: true,
-        pathRewrite: {
-          '^/wt': ''
-        }
+        changeOrigin: true
       }
     }
   }
