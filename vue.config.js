b23e249eaa48627591c164eccf6044eaacb008d1
wenbo.chang <wenbo.chang@huolala.cn>
2025-02-18 19:38:07 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,13 @@
+const fs = require('fs');
 module.exports = {
   publicPath: '/wt/',
   devServer: {
     host: '0.0.0.0',
     port: 5656,
     allowedHosts: 'all'
-  }
+  },
+  https: {
+    key: fs.readFileSync('/usr/local/nginx/key.pem'),
+    cert: fs.readFileSync('/usr/local/nginx/cert.pem'),
+  } 
 };
