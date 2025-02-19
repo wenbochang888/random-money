174e8633a77ab52a25462c3f1e28649c239daac7
wenbo.chang <wenbo.chang@huolala.cn>
2025-02-19 20:23:29 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,14 @@
 const fs = require('fs');
-const express = require('express');
-const https = require('https');
-const http = require('http');
-
-const app = express();
-const httpsOptions = {
-  key: fs.readFileSync('/usr/local/nginx/key.pem'),
-  cert: fs.readFileSync('/usr/local/nginx/cert.pem')
-};
 
 module.exports = {
   publicPath: '/wt/',
   devServer: {
     host: '0.0.0.0',
     port: 5656,
-    https: httpsOptions,
-    allowedHosts: 'all',
-    before: function(app, server, compiler) {
-      // Create an HTTP server
-      http.createServer(app).listen(5657, () => {
-        console.log('HTTP server running on port 5657');
-      });
-    }
+    https: {
+      key: fs.readFileSync('/usr/local/nginx/key.pem'),
+      cert: fs.readFileSync('/usr/local/nginx/cert.pem')
+    },
+    allowedHosts: 'all'
   }
 };
