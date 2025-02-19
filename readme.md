68102ef04e8f0e91f39fc9332e6ddaefd3325b81
wenbo.chang <wenbo.chang@huolala.cn>
2025-02-19 19:28:59 +0800

    add

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 1. nohup npm run serve > /root/logs/vue/dm.log 2>&1 &
 2. npm run serve 
+
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
   devServer: {
     host: '0.0.0.0',
     port: 5656,
+    https: true,
     allowedHosts: 'all'
   }
 };
