a1845fe1e75e99506b3df558c142ffb65e09c5e4
wenbo.chang <wenbo.chang@huolala.cn>
2025-02-19 19:53:38 +0800

    add

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 1. nohup npm run serve > /root/logs/vue/dm.log 2>&1 &
 2. npm run serve 
 
+
+5. 验证配置文件  /usr/local/nginx/sbin/nginx -t
+6. 重新加载配置文件  /usr/local/nginx/sbin/nginx -s reload
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,23 @@
+const fs = require('fs');
+const https = require('https');
+
 module.exports = {
   publicPath: '/wt/',
   devServer: {
     host: '0.0.0.0',
     port: 5656,
-    https: true,
-    allowedHosts: 'all'
+    https: {
+      key: fs.readFileSync('/usr/local/nginx/key.pem'),
+      cert: fs.readFileSync('/usr/local/nginx/cert.pem')
+    },
+    allowedHosts: 'all',
+    proxy: {
+      '^/wt': {
+        target: 'http://www.gdufe888.top:5656',
+        ws: true, // 启用 WebSocket 代理
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 };
